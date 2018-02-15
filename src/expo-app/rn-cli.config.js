39f0781b3d50c16e710da1f6073f8ba9daeb2895
Marcin Piwowarski <marcin.piwowarski@vehnet.com>
2018-02-15 14:15:12 +0000

    expo-app added to src. Changes made in rn-cli.config.js in rn proj

--- src/expo-app/rn-cli.config.js
+++ src/expo-app/rn-cli.config.js
@@ -6,11 +6,8 @@ module.exports = {
       // root of the (rn) project
       __dirname,
 
-      //getting access to `shared` 
+      // getting access to `shared` 
       path.join(__dirname, "../shared"),
-
-       //getting access to `web`
-      path.join(__dirname, "../web"),
 
       // finally the root proj node_modules for access to
       // shared dependencies
